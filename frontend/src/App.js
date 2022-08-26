c67d0df2bdaafd940140f0d12e80f02200260689
Rob Giering <robgiering@gmail.com>
2022-08-26 13:04:09 -0400

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,24 @@ import './main.scss';
 import Container from 'react-bootstrap/Container'
 import HomePage from './components/HomePage';
 import Navbarg from './components/Navbar';
+import ThemeSwitch from "./components/ThemeSwitch";
+import MiscellaneousCheese from "./components/MiscellaneousCheese";
+import { ThemeProvider } from "./hooks/useThemeContext";
 import React from 'react';
+import "./scss/bootstrap.scss";
 
 function App() {
   return (
     <div className="App">
+        <ThemeProvider>
         <Navbarg />
         <Container fluid>
         <HomePage/>
+          <ThemeSwitch />
+          <MiscellaneousCheese />
+        
         </Container>
+        </ThemeProvider>
     </div>
   );
 }
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,54 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import Button from 'react-bootstrap/Button';
+import React from 'react';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Logo from '../images/havit.png';
-
 const Navbarg = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#"><img
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+      <Navbar.Brand href="#"><img
               src={Logo}
               height="30"
               className="d-inline-block align-top"
               alt="Havit logo"
             /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
+          <Nav>
+            <Nav.Link href="#deets">More</Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
